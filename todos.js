5fdfe724187e5490acbc3073c6c20231929da6cf
AndyFernandes <andrezafernandes@alu.ufc.br>
2019-02-13 17:01:51 -0300

    [+] aula requisições ajax

--- todos.js
+++ todos.js
@@ -48,4 +48,51 @@ function saveToStorage(){
 	localStorage.setItem('list_todos', JSON.stringify(todos));
 };
 
+// ############ introdução a requisições ajax
+// var xhr = new XMLHttpRequest();
 
+// xhr.open('GET', 'https://api.github.com/users/andyfernandes');
+// xhr.send(null);
+
+// xhr.onreadystatechange = function(){
+// 	if(xhr.readyState === 4){
+// 		console.log(JSON.parse(xhr.responseText));
+// 	}
+// }
+
+// ########## promises
+// var minhaPromise = function(){
+// 	return new Promise(function(resolve, reject){
+// 		var xhr = new XMLHttpRequest();
+// 		xhr.open('GET', 'https://api.gitb.com/users/andyfernandes');
+
+// 		xhr.send(null);
+
+// 		xhr.onreadystatechange = function(){
+// 			if(xhr.readyState === 4){
+// 				if(xhr.status === 200){
+// 					resolve(JSON.parse(xhr.responseText));
+// 				} else {
+// 					reject('Erro na requisição');
+// 				}
+// 			} 
+// 		}
+// 	});
+// }
+
+// minhaPromise()
+// 	.then(function(response){
+// 		console.log(response);
+// 	})
+// 	.catch(function(error){
+// 		console.warn(error);
+// 	});
+
+// ####### USANDO AXIOS: encapsulamento do promises acima 
+// axios.get('https://api.github.com/users/andyfernandes')
+// 	.then(function(response){
+// 		console.log(response);
+// 	})
+// 	.catch(function(error){
+// 		console.warn(error);
+// 	});
